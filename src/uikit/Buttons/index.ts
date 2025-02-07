17b966c3b8705d2183b69b495954fc6cb8666754
udacosta-gp <udacosta@g-p.com>
2025-02-06 20:39:36 -0800

    updates

--- src/uikit/Buttons/index.ts
+++ src/uikit/Buttons/index.ts
@@ -1,5 +1,5 @@
-import { UIButton, UIButtonProps } from "./UIButon/UIButton";
-import { IconButton, IconButtonProps } from "./IconButton/IconButton";
+import { UIButton, UIButtonProps, UIButtonHandle } from './UIButon/UIButton';
+import { IconButton, IconButtonProps } from './IconButton/IconButton';
 
 export { UIButton, IconButton };
-export type { UIButtonProps, IconButtonProps };
+export type { UIButtonProps, IconButtonProps, UIButtonHandle };
