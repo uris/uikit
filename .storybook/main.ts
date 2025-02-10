7ec18d7db453373e18abc19b7ae464bc8a45468b
udacosta-gp <udacosta@g-p.com>
2025-02-10 14:44:35 -0800

    updates to public pathing of images

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,6 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
-  staticDirs: ['../public'],
+  staticDirs: [{ from: '../public', to: '/public' }],
 };
 export default config;
