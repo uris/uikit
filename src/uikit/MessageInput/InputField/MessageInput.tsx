75b74ec7942b0259b522bd410c3dfd21a47e09c6
uris <urisvirott@yahoo.com>
2025-02-12 00:32:20 -0800

    Revert "updates to messageinput placeholder spelling"
    This reverts commit 06bac24abaa525bef413f0cb0d04580137b6ec41.

--- src/uikit/MessageInput/InputField/MessageInput.tsx
+++ src/uikit/MessageInput/InputField/MessageInput.tsx
@@ -45,7 +45,7 @@ export function MessageInput(props: MessageInputProps) {
     focused = false,
     error = null,
     value = '',
-    placeholder = 'Ask me anything about HR compliance',
+    placeholder = 'Ask me anytning HR compliance',
     isStreaming = false,
     isFetching = false,
     isShort = true,
